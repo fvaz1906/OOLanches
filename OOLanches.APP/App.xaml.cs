b30d15a431bc40a8852224aa958855a0028ca130
Felipe Vaz <felipevaz@live.com>
2025-01-06 23:59:34 -0300

    aplicação .net maui criada e adicionado splash screen personalizado para android

---
+++ OOLanches.APP/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace OOLanches.APP
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ OOLanches.APP/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace OOLanches.APP
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
